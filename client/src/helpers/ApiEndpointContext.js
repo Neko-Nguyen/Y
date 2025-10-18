e291736eb92fcc6ec97845e267c9adc979ff5cac
Carameow <iywudla@gmail.com>
2025-10-18 13:13:22 +0800

    Use env

--- client/src/helpers/ApiEndpointContext.js
+++ client/src/helpers/ApiEndpointContext.js
@@ -1,6 +1,8 @@
 import { createContext } from "react";
-import { IS_LOCAL_SERVER, LOCAL_API, REMOTE_API } from "./Constants";
 
+const IS_LOCAL_SERVER = process.env.REACT_APP_IS_LOCAL_SERVER === 'true';
+const LOCAL_API = process.env.REACT_APP_LOCAL_API;
+const REMOTE_API = process.env.REACT_APP_REMOTE_API;
 const api = IS_LOCAL_SERVER ? LOCAL_API : REMOTE_API;
 
 export const ApiEndpointContext = createContext(api);
