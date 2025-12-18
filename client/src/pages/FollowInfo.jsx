089b42d8d567568d27b12494bd134655beecee26
Neko-Nguyen <iywudla@gmail.com>
2025-12-18 23:46:36 +0700

    Add follow info pages

---
+++ client/src/pages/FollowInfo.jsx
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { ApiEndpointContext } from "../helpers/ApiEndpointContext";
+import { getFollowInfo } from "../api/Follows";
+import ListOfUsers from "../components/ListOfUsers";
+
+function FollowInfo() {
+   let { info, id } = useParams();
+   const [followInfo, setFollowInfo] = useState({
+      followers: [],
+      followings: []
+   });
+   const api = useContext(ApiEndpointContext);
+   let navigate = useNavigate();
+
+   useEffect(() => {
+      async function fetchFollowInfo() {
+         const followInfo = await getFollowInfo(api, id);
+         setFollowInfo(followInfo);
+      };
+
+      fetchFollowInfo();
+   }, [api, id]);
+
+   return (
+      <div className="main home">
+         <div className="go-back">
+            <KeyboardBackspaceIcon 
+               sx={{ fontSize: 25 }} 
+               className="go-back-icon"
+               onClick={() => {
+                  navigate(`/profile/${id}`);
+               }}
+            />
+            <div className="go-back-text">Profile</div>
+         </div>
+
+         {info === "followings"
+         ? <ListOfUsers listOfUsers={followInfo.followings.map(u => u.Following)}/>
+         : <ListOfUsers listOfUsers={followInfo.followers.map(u => u.Follower)}/>
+         }
+      </div>
+   );
+}
+
+export default FollowInfo;
