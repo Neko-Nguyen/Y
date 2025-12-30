491c47a9219a6252683775fb9ab8c59679fb3d62
Carameow <iywudla@gmail.com>
2025-12-30 16:10:17 +0700

    Create user and listOfUsers objects

--- client/src/pages/object/ListOfUsers.jsx
+++ client/src/pages/object/ListOfUsers.jsx
@@ -1,10 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import UserObject from "./UserObject"
 
-function ListOfUsers({ listOfUsers }) {
-   return listOfUsers.map(user => (
-      <div>
-         {user.username}
-      </div>
-   ));
+const defaultInfo = {
+   listOfUsers: []
+}
+
+function ListOfUsers({ info = defaultInfo }) {
+   let navigate = useNavigate();
+
+   function navProfile(id) {
+      navigate(`/profile/${id}`);
+   }
+
+   return (<div className="list">
+      {info.listOfUsers.map(user => (
+         <div className="post home-post" onClick={() => navProfile(user.id)}>
+            <UserObject info={{ userObject: user }}/>
+         </div>
+      ))}
+   </div>);
 }
 
 export default ListOfUsers;
