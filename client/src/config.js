66e83850d51e24f351438e6d17cbb2b5b28aec6c
Carameow <iywudla@gmail.com>
2026-01-04 11:41:56 +0700

    Add remote api to config

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,6 @@ export const config = {
    isLocalServer: "true",
    localStorage: "localAccessToken",
    remoteStorage: "remoteAccessToken",
-   localAPI: "http://localhost:3001"
+   localAPI: "http://localhost:3001",
+   remoteAPI: "https://y-neko-nguyen-795f0d434f9d.herokuapp.com",
 };
--- client/src/helpers/ApiEndpointContext.js
+++ client/src/helpers/ApiEndpointContext.js
@@ -3,7 +3,7 @@ import { config } from "../config";
 
 const IS_LOCAL_SERVER = config.isLocalServer === 'true';
 const LOCAL_API = config.localAPI;
-const REMOTE_API = process.env.REACT_APP_REMOTE_API;
+const REMOTE_API = config.remoteAPI;
 const api = IS_LOCAL_SERVER ? LOCAL_API : REMOTE_API;
 
 export const ApiEndpointContext = createContext(api);
